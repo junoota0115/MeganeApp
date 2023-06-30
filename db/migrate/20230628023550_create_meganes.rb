7e9fef15c8b09bab89a16c5421d64f6ab4c4f17f
junoota0115 <j1u1n5@gmail.com>
2023-06-30 19:02:34 +0900

    登録後のviewなど

--- db/migrate/20230628023550_create_meganes.rb
+++ db/migrate/20230628023550_create_meganes.rb
@@ -1,7 +1,9 @@
 class CreateMeganes < ActiveRecord::Migration[7.0]
   def change
     create_table :meganes do |t|
-
+      t.string :title, null: false
+      t.string :name, null: false
+      t.text :content
       t.timestamps
     end
   end
